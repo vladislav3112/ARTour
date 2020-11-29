40dba8fd00e11fc14c26c3beb1d3a55c4f188f6c
dyushakolesov <dyushakolesov@mail.ru>
2020-11-29 18:59:43 +0300

    map and scan screen

---
+++ Assets/MyResourses/ImageRecognition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+public class ImageRecognition : MonoBehaviour
+{
+    private ARTrackedImageManager aRTrackedImageManager;
+
+    private void Awake()
+    {
+        aRTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    public void OnEnable()
+    {
+        aRTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    public void OnDisable()
+    {
+        aRTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args) {
+        foreach (var trakedImage in args.added) {
+            Debug.Log(trakedImage.name);
+        }
+    }
+
+}
---
+++ Assets/MyScripts/LoadScene.cs
@@ -0,0 +1,22 @@
+﻿namespace Mapbox.Unity.Map
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    public class LoadScene : MonoBehaviour
+    {
+        public string destinationSceneName;
+        // Start is called before the first frame update
+        string currScene;
+        public void OnClick()
+        {
+            currScene = SceneManager.GetActiveScene().name;
+            // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+            if (destinationSceneName != currScene)
+                SceneManager.LoadScene(destinationSceneName);
+        }
+
+    }
+}
